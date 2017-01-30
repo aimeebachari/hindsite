fe561470fa25bf18e2955abf8ead5a97339e6f6c
Aimee Bachari <aimeebachari@gmail.com>
2017-01-30 12:59:10 -0500

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+
+  root "home#index"
+  resources :home, only: [:index]
+
+  devise_for :users
+
+  resources :users, only: [:index, :show]
+
+  resources :historicalevents
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
