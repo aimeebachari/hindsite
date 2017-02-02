67d9c02cd8d8f4ba2f3ec63374ce4a92f13e6f1e
Aimee Bachari <aimeebachari@gmail.com>
2017-02-01 21:51:18 -0500

    form renders, still have to make sure it works

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -2,4 +2,20 @@ class TripsController < ApplicationController
   def index
     @trips = Trip.all
   end
+
+  def new
+    @trip = Trip.new
+    @events = Event.all
+  end
+
+  def create
+    @trip = Trip.new(trip_params)
+    @events = Event.all
+  end
+
+  private
+
+  def trip_params
+    params.require(:trip).permit(:name, :start_event, :end_event)
+  end
 end
