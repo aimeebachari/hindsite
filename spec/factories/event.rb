58bce1c274b34dd0c88edb4cd0b9ac91fe0e6f24
Aimee Bachari <aimeebachari@gmail.com>
2017-02-13 11:01:48 -0500

    Updated Readme to include mobile view.

--- spec/factories/event.rb
+++
@@ -1,9 +0,0 @@
-FactoryGirl.define do
-  factory :event do
-    association :user, factory: :user
-    sequence(:name) { |n| "user#{n}" }
-    sequence(:date) { |n| "177#{n}" }
-    sequence(:address) { |n| "123#{n} Summer St. Boston, MA 0211#{n}" }
-    description "A historical place."
-  end
-end
--- spec/factories/trip.rb
+++
@@ -1,8 +0,0 @@
-FactoryGirl.define do
-  factory :trip do
-    association :user, factory: :user
-    association :event, factory: :event
-    association :event, factory: :event
-    name "Fun Trip"
-  end
-end
